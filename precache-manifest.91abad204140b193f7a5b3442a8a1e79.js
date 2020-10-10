14dcc643fb1f20af5335a6b4e434f2eb0b2e259d
Layinka Jaji <layinkawebdev@gmail.com>
2020-10-10 13:08:40 +0100

    Updates

---
+++ precache-manifest.91abad204140b193f7a5b3442a8a1e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e19ed74f72f8920cd511fcf47e06872",
+    "url": "/sme-engage/index.html"
+  },
+  {
+    "revision": "1849af53fe197905cc33",
+    "url": "/sme-engage/static/css/main.31eb9020.chunk.css"
+  },
+  {
+    "revision": "3c096f1268ab5d8c674f",
+    "url": "/sme-engage/static/js/2.8174843d.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/sme-engage/static/js/2.8174843d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1849af53fe197905cc33",
+    "url": "/sme-engage/static/js/main.99fa9525.chunk.js"
+  },
+  {
+    "revision": "7f275296c36209fc18d4",
+    "url": "/sme-engage/static/js/runtime-main.110cc46f.js"
+  }
+]);
